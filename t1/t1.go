991517d7a3555ec8c5fcb4a68e12ad22011a7363
poonam1112 <poonam@nagarro.com>
2016-11-21 14:33:56 +0530

    tcp server

---
+++ t1/t1.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	// Listen for incoming connections.
+	l, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on tcp", ":8081")
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		defer conn.Close()
+		// Handle connections in a new goroutine.
+		handle := handleRequest(conn)
+		if handle != nil {
+			fmt.Println("operation failed")
+		}
+	}
+
+}
+
+func Check(response string) string {
+	if len(response) != 0 {
+		if (response == "Hello") || (response == "hello") {
+			response = "Hi"
+		} else if (response == "Name") || (response == "name") || (response == "Name??") {
+			response = "Chitty :) "
+		} else if (response == "Ok!! Goodbye") || (response == "Bye") || (response == "bye") {
+			response = "Bye"
+		} else {
+			response = "What?"
+		}
+
+	} else {
+		response = "Sorry??"
+	}
+	return response
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) error {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	for {
+		reqLen, err := conn.Read(buf)
+		if err != nil {
+			//fmt.Println("Error reading:", err.Error())
+			return errors.New("Received empty")
+		}
+		// output message received
+		request := string(buf[:reqLen-2])
+		fmt.Println("Client Request	:", request)
+
+		//reading response from server
+		response := Check(request)
+		fmt.Println("Server Response : ", response)
+
+		// Send a response back to person contacting us.
+		_, err1 := conn.Write([]byte(response + "\n"))
+		//checking if there is an error through a message
+		if err1 != nil {
+			return errors.New("Connection Full")
+		}
+		return nil
+	}
+	// Close the connection when you're done with it.
+	//conn.Close()
+
+}
---
+++ t1/t1_test.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+)
+
+//Struct for Conn type
+type myConn struct {
+	buf  []byte
+	r, w int
+}
+
+func (b *myConn) Read(p []byte) (n int, err error) {
+	//check if r=w means buffer is empty
+	if b.r == b.w {
+		errReadEmpty := errors.New("errReadEmpty")
+		return 0, errReadEmpty
+	}
+	//copy the buffer value to p
+	n = copy(p, b.buf[b.r:b.w])
+	b.r += n
+	if b.r == b.w {
+		b.r = 0
+		b.w = 0
+	}
+	return n, nil
+}
+
+//Function for close connection
+func (c *myConn) Close() error {
+
+	return nil
+}
+
+func (b *myConn) Write(p []byte) (n int, err error) {
+	// Slide existing data to beginning.
+	if b.r > 0 && len(p) > len(b.buf)-b.w {
+		copy(b.buf, b.buf[b.r:b.w])
+		b.w -= b.r
+		b.r = 0
+	}
+
+	// Write new data.
+	n = copy(b.buf[b.w:], p)
+	b.w += n
+	if n < len(p) {
+		err = errors.New("errWriteFull")
+	}
+	return n, err
+}
+
+func (b *myConn) LocalAddr() net.Addr {
+	//some code
+	return nil
+}
+func (b *myConn) RemoteAddr() net.Addr {
+	// some code
+	return nil
+}
+func (b *myConn) SetDeadline(t time.Time) error {
+	// some code
+	return nil
+}
+func (b *myConn) SetReadDeadline(t time.Time) error {
+	// some code
+	return nil
+}
+func (b *myConn) SetWriteDeadline(t time.Time) error {
+	// some code
+	return nil
+}
+
+//Testing for Read() Function
+func TestRead(t *testing.T) {
+
+	b := make([]byte, 4)
+	b[0] = 0x00
+	b[1] = 0x01
+	b[2] = 10
+	c := &myConn{buf: b, r: 0, w: 4}
+
+	//expectederr := errors.New("errReadEmpty")
+	rCount, err1 := c.Read(b)
+	if err1 != nil && rCount != 4 {
+		t.Fatal("failed to read ")
+	}
+	//fmt.Println("read count is : \n", rCount)
+	fmt.Println(b)
+}
+
+//Testing of Write() Function
+func TestWrite(t *testing.T) {
+
+	b := make([]byte, 4)
+	b[0] = 0x00
+	b[1] = 0x01
+	b[2] = 10
+	c := &myConn{buf: b, r: 0, w: 4}
+
+	rCount, err1 := c.Read(b)
+	if err1 != nil && rCount != 4 {
+		t.Fatal("failed to read ")
+	}
+	fmt.Println(b)
+
+	wCount, err := c.Write(b)
+	if err != nil && wCount != 4 {
+		t.Fatal("failed to write ")
+	}
+	fmt.Println(c.buf)
+}
+
+//Testing for Check function (gives reponse to a message)
+func TestCheck(t *testing.T) {
+
+	expected := "Hello"
+	actual := "Hi"
+	msg := Check(expected)
+	fmt.Println(msg)
+	if msg != actual {
+		t.Fatal("failed to Interpret message ")
+	}
+
+	/*Expected := ""
+	Actual := "Hello"
+	msg = Check(Expected)
+	fmt.Println(msg)
+	if msg != Actual {
+		t.Fatal("failed to Interpret message ")
+	}*/
+}
+
+//testing handlerequest function which handle the connection
+func TestHandleRequest(t *testing.T) {
+
+	b := make([]byte, 20)
+
+	c := &myConn{buf: b, r: 0, w: 4}
+
+	handle := handleRequest(c)
+	if handle != nil {
+		t.Fatal("failed to Handle Operation ")
+	}
+}
